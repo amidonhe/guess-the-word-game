0df696ff98df8841cc0af35f4caaf48605d29111
Hannah Amidon <93405611+amidonhe@users.noreply.github.com>
2023-03-03 16:14:02 -0600

    Added parameters to user guesses

--- guess-the-word-main/js/script.js
+++ guess-the-word-main/js/script.js
@@ -8,6 +8,7 @@ const message = document.querySelector(".message");
 const playAgainButton = document.querySelector("play-again hide");
 
 const word = "magnolia";
+const guessedLettersArray = [];
 
 const placeholder = function (word) {
     const placeholderLetters = [];
@@ -22,7 +23,35 @@ placeholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const input = letterInput.value;
-    console.log(input);
+    message.innerText = "";
+    const guessInput = letterInput.value;
+    const validGuess = validateInput(guessInput);
+    //console.log(guessInput);
+    if (validGuess) {
+        makeGuess(guessInput);
+    }
     letterInput.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Please enter a letter.";
+    } else if (input.length != 1) {
+        message.innerText = "Please enter one letter per turn.";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Please enter a valid character.";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guessInput) {
+    guessInput = guessInput.toUpperCase();
+    if (guessedLettersArray.includes(guessInput)) {
+        message.innerText = "You already guessed that letter. Try again.";
+    } else {
+        guessedLettersArray.push(guessInput);
+        console.log(guessedLettersArray);
+    }
+};
